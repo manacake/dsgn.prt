30ca38b660ac07471a054e79fc8ce3d0a8ef77ac
Tina Zhao <tinayzhao@gmail.com>
2017-10-16 18:33:16 -0700

    add font awesome

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import './styles/main';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
